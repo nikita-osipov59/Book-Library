20cc325bab56aa5b7311f32ec8c0faa591121602
Nikita <nikita.osipov59@yandex.ru>
2024-01-14 15:47:04 +0300

    Added BookForm, BookList and Filter components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import BookList from "./components/BookList/BookList";
+import BookForm from "./components/BookForm/BookForm";
+import Filter from "./components/Filter/Filter";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="app">
-        <header className="app-header">
-          <h1>Book Library app</h1>
-        </header>
-      </div>
-    </>
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library app</h1>
+      </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          {/* <Filter /> */}
+          <BookList />
+        </div>
+      </main>
+    </div>
   );
 }
 
