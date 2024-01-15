8714dd857f32099f590101d293f699193ef3c6e9
Nikita <nikita.osipov59@yandex.ru>
2024-01-15 16:01:01 +0300

    added Delete Book id

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
-import addBook from "../../redux/books/actionCreators";
+import { addBook, deleteBook } from "../../redux/books/actionCreators";
 import "./BookForm.css";
 
 const BookForm = () => {
@@ -20,7 +20,7 @@ const BookForm = () => {
       dispatch(addBook(book));
       setTitle("");
       setAuthor("");
-    }
+    } else dispatch(deleteBook());
   };
 
   return (
--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,8 +1,16 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "./BookList.css";
+import { deleteBook } from "../../redux/books/actionCreators";
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id));
+  };
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -13,8 +21,15 @@ const BookList = () => {
           {books.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
-                {++i}.{book.title} by <strong>{book.author}</strong>
+                {++i}. {book.title} by <strong>{book.author}</strong>
               </div>
+              <button
+                key={i}
+                onClick={() => handleDeleteBook(book.id)}
+                className="book-actions"
+              >
+                Delete
+              </button>
             </li>
           ))}
         </ul>
