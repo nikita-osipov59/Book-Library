45bcdd07eda0070dd404771fd6ba2369200bfed8
Nikita <nikita.osipov59@yandex.ru>
2024-01-15 12:52:32 +0300

    initialize Redux, add and display books

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
